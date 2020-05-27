b0519c85281f5c7bae440ee6286e081ae15b3406
yejin25 <yjin9187@naver.com>
2020-05-27 20:11:08 +0900

    Login 구현

---
+++ app/src/main/java/kr/ac/jbnu/se/mobile/oneulro/LoginData.java
@@ -0,0 +1,9 @@
+package kr.ac.jbnu.se.mobile.oneulro;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginData {
+    public static FirebaseAuth firebaseAuth = null;
+    public static GoogleSignInClient mGoogleSignInClient = null;
+}
---
+++ app/src/main/java/kr/ac/jbnu/se/mobile/oneulro/MainActivity.java
@@ -0,0 +1,16 @@
+package kr.ac.jbnu.se.mobile.oneulro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
